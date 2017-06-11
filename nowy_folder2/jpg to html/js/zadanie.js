b4b7665975bd3150eea035e6f2cc208d4459f1f5
Monika Wiosna <mwiosna.zielinska@gmail.com>
2017-06-11 18:20:15 +0200

    zoom

--- nowy_folder2/jpg to html/js/zadanie.js
+++ nowy_folder2/jpg to html/js/zadanie.js
@@ -4,36 +4,47 @@ document.addEventListener("DOMContentLoaded", function() {
 
 var hidden = false;
 
-setInterval(function(){
-    document.querySelector("a.contest").style.visibility= hidden ? "visible" : "hidden";
-    hidden = !hidden;
-},3000);
+setInterval(function() {
+document.querySelector("a.contest").style.visibility = hidden ? "visible" : "hidden";
+hidden = !hidden;
+}, 3000);
 
 
+//ZOOM part
 
-
-var allLi = document.querySelectorAll('.gallery img');
-var images = document.querySelectorAll('img');
+var allLi = document.querySelectorAll('.gallery img'); //robimy na img nie mamy potrzeby
+//na li poniewaz naszym thisem,naszym kontekstem od razu jest img
+//var images = document.querySelectorAll('img');
 
 for (var i = 0; i < allLi.length; i++) {
-    allLi[i].addEventListener('click', function(el) {
-        el.preventDefault();
-        //console.log(this);
-        // var new_url = this.querySelector('img[src]').getAttribute('src');
-        var _tag = document.querySelector('body');
-        var new_div = document.createElement('div');
-        var new_img = document.createElement('img');
-        var button = document.createElement('button');
-        // new_img.setAttribute('src', new_url);
-        button.classList.add('close');
-        button.innerText = 'close';
-        new_div.classList.add('fullScreen');
-        new_div.appendChild(button);
-        new_div.appendChild(new_img);
+allLi[i].addEventListener('click', function(el) {
+    el.preventDefault();
+    //przy kliknieciu na ktorekolwiek ze zdjec dodajemy nowy element
+
+    console.log(this);
+    var new_url = this.getAttribute('src');
+    var new_tag = document.querySelector('body'); //musimy powiedziec elementowi gdzie ma dodac
+    var new_div = document.createElement('div');
+    var new_img = document.createElement('img');
+    var button = document.createElement('button');
+    new_img.setAttribute('src', new_url);
+    button.classList.add('close');
+    button.innerText = 'close';
+    new_div.classList.add('fullScreen');
+    new_div.appendChild(button);
+    new_div.appendChild(new_img);
+
+    new_tag.appendChild(new_div);
 
 
+    var closeBtn = document.querySelectorAll('.close');
+    closeBtn.forEach((el) => {
+        el.addEventListener('click', function() {
+            console.log(el.parentNode.parentNode.removeChild(new_div));
+        })
     })
-
+    //wchodzi sie wyzej niz jest ten element , nie na poziom element
+})
 }
 
 })
